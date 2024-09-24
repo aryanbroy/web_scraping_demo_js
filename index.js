119c294638703296ca1b900df005e76472ea6695
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-25 00:02:31 +0530

    get details from a website and store it in your local storage folder

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-import { PlaywrightCrawler } from "crawlee";
+import { PlaywrightCrawler, Dataset } from "crawlee";
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ page, request, enqueueLinks }) => {
     console.log(`Processing url: ${request.url}`);
 
     if (request.label === "DETAIL") {
+      const urlPart = request.url.split("/").slice(-1);
+      const manufacturer = urlPart[0].split("-")[0];
+
+      const title = await page.locator(".product-meta h1").textContent();
+      const sku = await page.locator("span.product-meta__sku").textContent();
+
+      const priceElement = page
+        .locator("span.price")
+        .filter({ hasText: "$" })
+        .first();
+      const currentPriceString = await priceElement.textContent();
+      const rawPrice = currentPriceString.split("$")[1];
+      const price = Number(rawPrice.replace(",", ""));
+
+      const inStockElement = page
+        .locator("span.product-form__inventory")
+        .filter({ hasText: "In stock" })
+        .first();
+      const inStock = (await inStockElement.count()) > 0;
+
+      const results = {
+        url: request.url,
+        manufacturer,
+        title,
+        sku,
+        currentPrice: price,
+        available: inStock,
+      };
+      await Dataset.pushData(results);
     } else if (request.label === "CATEGORY") {
-      await page.waitForSelector(".product-item");
+      await page.waitForSelector(".product-item > a");
 
       await enqueueLinks({
-        selector: ".product-item",
+        selector: ".product-item > a",
         label: "DETAIL",
       });
 
