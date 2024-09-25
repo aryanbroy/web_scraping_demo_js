c15f42ebdaa00c32dccaaceec690e80d2163f02e
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-25 16:08:52 +0530

    refactor code

--- index.js
+++ index.js
@@ -1,63 +1,15 @@
-import { PlaywrightCrawler, Dataset } from "crawlee";
+import { PlaywrightCrawler, log } from "crawlee";
+import { router } from "./routes.js";
 
+// This is better set with CRAWLEE_LOG_LEVEL env var
+// or a configuration option. This is just for show 😈
+log.setLevel(log.LEVELS.DEBUG);
+
+log.debug("Setting up crawler.");
 const crawler = new PlaywrightCrawler({
-  requestHandler: async ({ page, request, enqueueLinks }) => {
-    console.log(`Processing url: ${request.url}`);
-
-    if (request.label === "DETAIL") {
-      const urlPart = request.url.split("/").slice(-1);
-      const manufacturer = urlPart[0].split("-")[0];
-
-      const title = await page.locator(".product-meta h1").textContent();
-      const sku = await page.locator("span.product-meta__sku").textContent();
-
-      const priceElement = page
-        .locator("span.price")
-        .filter({ hasText: "$" })
-        .first();
-      const currentPriceString = await priceElement.textContent();
-      const rawPrice = currentPriceString.split("$")[1];
-      const price = Number(rawPrice.replace(",", ""));
-
-      const inStockElement = page
-        .locator("span.product-form__inventory")
-        .filter({ hasText: "In stock" })
-        .first();
-      const inStock = (await inStockElement.count()) > 0;
-
-      const results = {
-        url: request.url,
-        manufacturer,
-        title,
-        sku,
-        currentPrice: price,
-        available: inStock,
-      };
-      await Dataset.pushData(results);
-    } else if (request.label === "CATEGORY") {
-      await page.waitForSelector(".product-item > a");
-
-      await enqueueLinks({
-        selector: ".product-item > a",
-        label: "DETAIL",
-      });
-
-      const nextButton = page.$("a.pagination__next link");
-      if (nextButton) {
-        await enqueueLinks({
-          selector: "a.pagination__next link",
-          label: "CATEGORY",
-        });
-      }
-    } else {
-      await page.waitForSelector(".collection-block-item");
-
-      await enqueueLinks({
-        selector: ".collection-block-item",
-        label: "CATEGORY",
-      });
-    }
-  },
+  // Instead of the long requestHandler with
+  // if clauses we provide a router instance.
+  requestHandler: router,
 });
 
 await crawler.run(["https://warehouse-theme-metal.myshopify.com/collections"]);
